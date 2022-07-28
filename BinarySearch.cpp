ec99337ecda296061c8485e703718a851f8d99fd
Sireesha Ch <chssireesha@gmail.com>
2022-07-28 23:23:36 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,22 @@
+/* Some important questions in binary search
+
+1. Binary search - with equality comparison
+2. Without equalality comparison -
+   - flooring the mid element and return l in the end,
+   - capping the mid element and return r in the end.
+3. What to return when key is not found in search - (-1 or closest element? lesser or greater)
+4. Duplicates
+   - return any match
+   - return leftmost
+   - return rightmost
+   - both
+5. return exact match or least greater than (upper bound) the key element in case match not found
+6. return exact match or greatest element to left (lower bound) of match in case match not found
+7. Return k closest elements to match
+8. Find the element in rotated sorted array
+9. Find the lowest element of rotated sorted array
+10. Find element closest in Binary search tree
+
+
+Test cases:  odd and even, visualize with 3, 2, 1 elements - exact match and no match
+*/
